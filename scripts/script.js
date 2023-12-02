f135b3615142c4af9055c973e5cf080089b0b0e3
Caroline <41377264+carpfairy@users.noreply.github.com>
2023-12-02 05:31:34 -0500

    Update 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let clothingTypeList = ["Shirt", "Blouse", "Pants", "Blazer", "Jeans", "Cardigan/Sweater", "Jacket/Coat", "Skirt", "Sweatshirt", "Undergarments", "Lingerie"]
+let materialTypeList = ["Acrylic", "Cashmere", "Cotton", "Down", "Elastene", "Genuine Leather", "Linen", "Nylon", "Polyester", "Polyurethane", "Care", "Rayon", "Silk", "Spandex", "Viscose", "Wool"]
 
 function clothingTypeDropDown(){
     let cLength = clothingTypeList.length;
@@ -64,20 +65,107 @@ if(window.location.href.indexOf("main") > -1){
 
 let formresults;
 let results;
-
+let createDivMat = [];
+let divIdCounter = 1;
 if(window.location.href.indexOf("results") > -1){
     //Create condition for empty input
-
+    
     //Non-empty input
     formresults = JSON.parse(localStorage.getItem('form'));
     results = Object.values(formresults);
     console.log(results);
 
     let clothtype = results[0];
+    let materialArr = results[1].split(',');
+
     let clothtypefile1 = "<object data=fabric-data/clothtype-"
     let clothtypefile2 = ".txt width=100%></object>"
     let clothtypedesc = clothtypefile1 + clothtype + clothtypefile2;
 
     document.getElementById("clothtype").innerHTML = clothtype + clothtypedesc;
-    document.getElementById("")
+    materialChecker(materialArr)
+    
+}
+
+function materialChecker(matArr){
+    console.log(matArr);
+
+    for(let i=0; i<matArr.length; i++){
+        console.log(matArr[i]);
+        for(let j=0; j<materialTypeList.length; j++){
+            if(matArr[i].includes(materialTypeList[j])){
+                console.log("found" + matArr[i]);
+                createDivMat.push(materialTypeList[j]);
+            }
+        }
+    }
+    console.log(createDivMat);
+}
+
+function createRowDiv(fabricname){
+    let fabricString = fabricname.toString();
+    let thisDiv=document.getElementById("table-container");
+    let container=document.createElement("div");
+    container.classList.add("results-row");
+    container.setAttribute("id", "'" + fabricString + "'");
+    thisDiv.appendChild(container);
+
+}
+
+function createColDiv(counter, fabric){
+    let container=document.querySelectorAll(".results-row")[counter];
+    
+    let smallCol=document.createElement("div");
+    smallCol.classList.add('results-table-small');
+    container.appendChild(smallCol);
+
+    let largeCol=document.createElement("div");
+    largeCol.classList.add('results-table-large');
+    container.appendChild(largeCol);
+    divIdCounter+= 1;
+}
+
+function createColDivDesc(counter, fab){
+    let thisDiv=document.querySelectorAll('.results-table-large')[counter];
+    
+    let topic=document.createElement("div");
+    topic.classList.add('results-material-title');
+    thisDiv.appendChild(topic);
+    topic.innerHTML="<b>Fabric:</b> " + fab;
+
+    let topicDesc=document.createElement("div");
+    topicDesc.classList.add('results-material-desc');
+    thisDiv.appendChild(topicDesc);
+    topicDesc.innerHTML = "Fabric description"
+    
+    let enviro=document.createElement("div");
+    enviro.classList.add('results-material-title');
+    thisDiv.appendChild(enviro);
+    enviro.innerHTML="<b>Environmental Impact:</b> "
+
+    let enviroDesc=document.createElement("div");
+    enviroDesc.classList.add('results-material-desc');
+    thisDiv.appendChild(enviroDesc);
+    enviroDesc.innerHTML = "Environmental Impact"
+
+    let care=document.createElement("div");
+    care.classList.add('results-material-title');
+    thisDiv.appendChild(care);
+    care.innerHTML = "<b>Care:</b>"
+
+    let careDesc=document.createElement("div");
+    careDesc.classList.add('results-material-desc');
+    thisDiv.appendChild(careDesc);
+    careDesc.innerHTML = "Care description"
+            
+}
+
+
+
+let counter=0;
+for(i=0; i<createDivMat.length; i++){
+    createRowDiv(createDivMat[i])
+    createColDiv(counter, createDivMat[i])
+    createColDivDesc(counter, createDivMat[i])
+    counter+=1;
 }
